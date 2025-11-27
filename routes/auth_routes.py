ee9ed8223ced8634fce3d61764f0c8ba7f7cf5c2
tcoders16 <tcoders08164002@gmail.com>
2025-11-26 21:29:58 -0500

    auth

---
+++ routes/auth_routes.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+import os
+import jwt
+import datetime
+
+router = APIRouter()
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+JWT_SECRET = os.getenv("JWT_SECRET")
+
+USERS = [
+    {
+        "username": os.getenv("ADMIN_1_USERNAME"),
+        "password": os.getenv("ADMIN_1_PASSWORD")
+    },
+    {
+        "username": os.getenv("ADMIN_2_USERNAME"),
+        "password": os.getenv("ADMIN_2_PASSWORD")
+    }
+]
+
+@router.post("/api/login")
+def login(req: LoginRequest):
+    for user in USERS:
+        if user["username"] == req.username and user["password"] == req.password:
+            token = jwt.encode(
+                {
+                    "username": req.username,
+                    "role": "admin",
+                    "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=12),
+                },
+                JWT_SECRET,
+                algorithm="HS256"
+            )
+            return {"token": token}
+
+    raise HTTPException(status_code=401, detail="Invalid username or password")
--- routes/predict_routes.py
+++ routes/predict_routes.py
@@ -1,6 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Header
+from fastapi import  HTTPException
+import jwt
+import os
 from controller.predict_controller import predict
 
+
 router = APIRouter()
+
+JWT_SECRET = os.getenv("JWT_SECRET")
+
+def verify_token(authorization: str = Header(None)):
+    if not authorization:
+        raise HTTPException(status_code=401, detail="Missing token")
+
+    token = authorization.replace("Bearer ", "")
+
+    try:
+        decoded = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+        return decoded  
+    except Exception:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
 
 router.post("/predict")(predict)
